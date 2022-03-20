d945ca0b12563b47856caca01ceddc7505f24e07
beez2022 <101962834+beez2022@users.noreply.github.com>
2022-03-20 14:40:06 +0800

    Original Copy

---
+++ api_get_temp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 20 14:30:12 2022
+
+@author: beekheng
+"""
+
+from urllib.request import urlopen
+import json
+
+def get_weather(sID):
+     sock = urlopen("https://api.data.gov.sg/v1//environment/air-temperature")
+     result = sock.read()
+     sock.close
+     weather = json.loads(result)
+
+     listof_temp = weather['items'][0]['readings']
+     for info in listof_temp:
+         if info['station_id'].lower() == sID.lower():
+             return(info)
+     return(False)
